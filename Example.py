21bab48a64c0c76cdac57fc7d355e7f07e42d9ee
Pablo Zuñiga <pabzuniga@alumnos.uai.cl>
2022-06-20 22:23:21 -0400

    se añade archivo

---
+++ Example.py
@@ -0,0 +1,74 @@
+import numpy as np
+import sys
+
+# Check if cell (x, y) is valid or not
+def is_valid_cell(x, y, N):
+    if x < 0 or y < 0 or x >= N or y >= N:
+        return False
+
+    return True
+
+def find_paths_util(maze, source, destination, visited, path, paths):
+  """Find paths using Breadth First Search algorith """
+  # Done if destination is found
+  if source == destination:
+    paths.append(path[:])  # append copy of current path
+    return
+
+  # mark current cell as visited
+  N = len(maze)
+  x, y = source
+  visited[x][y] = 1
+
+  # if current cell is a valid and open cell,
+  if is_valid_cell(x, y, N) and maze[x][y]:
+    # Using Breadth First Search on path extension in all direction
+
+    # go right (x, y) --> (x + 1, y)
+    if x + 1 < N and (not (visited[x + 1][y] != 0)):
+    # if x + 1 < N :
+      # print("1-visited[x + 1][y] : ", visited[x + 1][y], "if1 x :", x, "if1 y :", y)
+      path.append((x + 1, y))
+      find_paths_util(maze,(x + 1, y), destination, visited, path, paths)
+      path.pop()
+
+    # go left (x, y) --> (x - 1, y)
+    if x - 1 >= 0 and (not (visited[x - 1][y] != 0)):
+      print("2-visited[x - 1][y] : ",  visited[x - 1][y], "if1 x :", x, "if1 y :", y)
+      path.append((x - 1, y))
+      find_paths_util(maze, (x - 1, y), destination, visited, path, paths)
+      path.pop()
+
+    # go up (x, y) --> (x, y + 1)
+    if y + 1 < N and (not (visited[x][y + 1] != 0)):
+      print("3-visited[x][y + 1] : ", visited[x][y + 1], "if1 x :", x, "if1 y :", y)
+      path.append((x, y + 1))
+      find_paths_util(maze, (x, y + 1), destination, visited, path, paths)
+      path.pop()
+
+    # go down (x, y) --> (x, y - 1)
+    if y - 1 >= 0 and (not (visited[x][y - 1] != 0)):
+      print("4-visited[x][y - 1] : ", visited[x][y - 1], "if1 x :", x, "if1 y :", y)
+      path.append((x, y - 1))
+      find_paths_util(maze, (x, y - 1), destination, visited, path, paths)
+      path.pop()
+
+    # Unmark current cell as visited
+  visited[x][y] = 0
+
+  return paths
+
+def find_paths(maze, source, destination):
+  """ Sets up and searches for paths"""
+  N = len(maze) # size of Maze is N x N
+
+  # 2D matrix to keep track of cells involved in current path
+  visited = [[0]*N for _ in range(N)]
+
+  path = [source]
+  paths = []
+  paths = find_paths_util(maze, source, destination, visited, path, paths)
+  print("Paths with '->' separator between maze cell locations")
+
+  return paths
+
--- funciones.py
+++ funciones.py
@@ -19,32 +19,3 @@ def buscarEntrada(matriz, contador):
 def buscarSalida(matriz,contador):
     valor = matriz[contador][-1]
     return contador if valor == 0 else buscarSalida(matriz, contador + 1)
-
-"""
-def findPaths(matriz, path, i, j):
-
-    M = len(matriz)
-    N = len(matriz[0])
-    visited = [[0] * N for _ in range(N)]
-    if (i == M - 1 and j == N - 1):
-        path.append(matriz[i][j])
-        print(path)
-        path.pop()
-        return
-
-    path.push()
-
-    # move right
-    if ((i >= 0 and i < M and j + 1 >= 0 and j + 1 < N)):
-        findPaths(matriz, path, i, j + 1)
-
-
-    #move down
-    if ((i + 1 >= 0 and i + 1 < M and j >= 0 and j < N)):
-        findPaths(matriz, path, i + 1, j)
-
-
-    # backtrack: remove the current cell from the path
-    path.pop()
-"""
-
