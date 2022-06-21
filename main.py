bbeca0398636d47f3474d1a51882e4b6c05e370f
Pablo Zuñiga <pabzuniga@alumnos.uai.cl>
2022-06-20 22:16:00 -0400

    se añade archivo example

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 from funciones import *
-
+from Example import *
 
 def main():
-    lista = []
+    paths = []
     matriz = loadMatrix()
 
     entrada = buscarEntrada(matriz, 0)
-    aniadirBordes(lista, entrada)
+    #aniadirBordes(path, entrada)
 
     salida = buscarSalida(matriz,0)
-    aniadirBordes(lista,salida)
+    #aniadirBordes(path,salida)
 
-    print(lista)
+    #findPaths(matriz, path, entrada, 0)
+    N = len(matriz)
+
+    maze = np.array(matriz)
+    # print(matriz)
+
+    maze[maze == 1] = 2
+    maze[maze == 0] = 1
+    maze[maze == 2] = 0
+    print(maze)
+    # Start point and destination
+    source = (entrada, 0)  # top left corner
+    destination = (salida, N-1)  # bottom right corner
+
+    # Find all paths
+    paths = find_paths(maze, source, destination)
+
+
+
+    print(paths)
 
 
 
